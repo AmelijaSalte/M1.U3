34a1d13f4c8fa26791600bf3250610cb53fd68cd
AmelijaSalte <140316202+AmelijaSalte@users.noreply.github.com>
2025-04-19 10:22:02 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,38 @@
+import telebot
+from bot_logic import gen_pass, gen_emodji, flip_coin
+    
+# Замени 'TOKEN' на токен твоего бота
+# Этот токен ты получаешь от BotFather, чтобы бот мог работать
+bot = telebot.TeleBot("7684836427:AAGldBywwvS68MZvYohrF1xFuIuF_8NW5dc")
+    
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+    
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+    
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(commands=['generate_password'])
+def send_password(message):
+    bot.reply_to(message, gen_pass(10))
+
+@bot.message_handler(commands=['emodji'])
+def send_emodji(message):
+    emodji = gen_emodji()
+    bot.reply_to(message, f"Вот эмоджи': {emodji}")
+
+@bot.message_handler(commands=['coin'])
+def send_coin(message):
+    coin = flip_coin()
+    bot.reply_to(message, f"Монетка выпала так: {coin}")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
